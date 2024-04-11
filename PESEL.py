319e3f6618d5e391b7770727f5f87f4cb0c5320e
jasiorek24 <Jasiorek@onet.pl>
2024-04-11 14:18:42 +0200

    .

---
+++ PESEL.py
@@ -0,0 +1,33 @@
+import random
+import math
+
+a=1
+b=2
+c=3
+d=4
+e=5
+A=6
+B=7
+C=8
+D=9
+E=10
+F=11
+
+f=int(input("Podaj pierwszą liczbę PESELU: "))
+g=int(input("Podaj drugą liczbę PESELU: "))
+h=int(input("Podaj trzecią liczbę PESELU: "))
+i=int(input("Podaj czwartą liczbę PESELU: "))
+j=int(input("Podaj piątą liczbę PESELU: "))
+K=int(input("Podaj piątą liczbę PESELU: "))
+L=int(input("Podaj szóstą liczbę PESELU: "))
+M=int(input("Podaj siódmą liczbę PESELU: "))
+N=int(input("Podaj ósmą liczbę PESELU: "))
+O=int(input("Podaj dziewiątą liczbę PESELU: "))
+P=int(input("Podaj dziesiątą liczbę PESELU: "))
+R==int(input("Podaj jedenastą liczbę PESELU: "))
+
+
+if a==f and b==g and c==h and d==i and e==j and :
+    print("PESEL poprawny")
+else:
+    print("Podany PESEL jest niepoprawny")
